b481da9a054b79327def93b6d47a0d15ed520ad6
fwnnio0 <1725110187@utectulancingo.edu.mx>
2026-06-30 13:05:46 +0000

    UPDATED agenda123

--- controllers/lista_contactos.py
+++ controllers/lista_contactos.py
@@ -5,27 +5,37 @@ render = web.template.render('views', base='layout')
 
 class ListaContactos:
 
-    def conectar(self):
+    def consultarContactos(self):
         try:
-            conexion = sqlite3.connect("sql/agenda.db")
+            conexion = sqlite3.connect("sql/agenda.sqlite")
             conexion.row_factory = sqlite3.Row
-            return conexion
-        except Exception as error:
-            print(f"ERROR 100: {error.args}")
-            return None
-
-    def listaContactos(self):
-        try:
-            conexion = self.conectar()
             cursor = conexion.cursor()
-            sql = "SELECT * FROM contactos"
-            cursor.execute(sql)
-            contactos = cursor.fetchall()
-            return contactos
+            query = "SELECT * FROM contactos;"
+            cursor.execute(query)
+            resultado = cursor.fetchall()
+
+            datos = []
+            for fila in resultado:
+                contacto = {
+                    "id_contacto":fila[0],
+                    "nombre":fila[1],
+                    "primer_apellido":fila[2],
+                    "segundo_apellido":fila[3],
+                    "email":fila[4],
+                    "telefono":fila[5]
+                }
+                datos.append(contacto)
+
+            conexion.close()
+            print(datos)
+            return datos
+        except sqlite3.Error as error:
+            print(f"ERROR 100: {error.args}")
+            return []
         except Exception as error:
             print(f"ERROR 101: {error.args}")
-            return None
+            return []
 
     def GET(self):
-        print(self.listaContactos())
-        return render.lista_contactos()
+        contactos = self.consultarContactos()
+        return render.lista_contactos(contactos)
