551030bed8b8c68043c07e699061ffa800d3666b
fwnnio0 <1725110187@utectulancingo.edu.mx>
2026-06-30 23:06:31 +0000

    UPDATED agenda12

--- controllers/ver_contacto.py
+++ controllers/ver_contacto.py
@@ -5,5 +5,10 @@ render = web.template.render('views', base='layout')
 
 class VerContacto:
     def GET(self, id_contacto):
-        print(f"ID_CONTACTO: {id_contacto}")
-        return render.ver_contacto()
+        conexion = _sqlite3.connect('sql/agenda.sqlite')
+        cursor = conexion.cursor()
+        cursor.execute("SELECT * FROM contactos WHERE id_contacto = ?", (id_contacto,))
+        contacto = cursor.fetchone()
+        conexion.close()
+
+        return render.ver_contacto(contacto)
