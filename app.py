4ae0c5a1c30a1758829889127eed74b24775cb57
fwnnio0 <1725110187@utectulancingo.edu.mx>
2026-07-02 14:42:06 +0000

    UPDATED agenda10

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ import web
 urls = (
     '/', 'controllers.index.Index',
     '/lista_contactos', 'controllers.lista_contactos.ListaContactos',
-    '/insertar_contacto', 'controllers.insertar_contacto.InsertarContacto',
-    '/ver_contacto/(.*)', 'controllers.ver_contacto.VerContacto'
+    '/insertar_contacto/(.*)', 'controllers.insertar_contacto.InsertarContacto',
+    '/ver_contacto/(.*)', 'controllers.ver_contacto.VerContacto',
+    '/borrar_contacto/(.*)', 'controllers.borrar_contacto.BorrarContacto',
+    '/editar_contacto/(.*)', 'controllers.editar_contacto.EditarContacto'
 )
 
 app = web.application(urls, globals())
